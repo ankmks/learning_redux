35bfe04d44475c21bf1a8cdfa18fdd52ab230c06
ankmks <anukool.mks@gmail.com>
2018-08-14 21:05:31 +0700

    First commit.

---
+++ hello-redux/README.md
@@ -0,0 +1,6 @@
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+
+# Hello Redux
+
+```git clone https://github.com/ohansemmanuel/hello-redux.git```
+
---
+++ hello-redux/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import HelloWorld from "./HelloWorld";
+import ButtonGroup from "./ButtonGroup";
+import { store } from "./store";
+//import reducer from "./reducers";
+//import { createStore } from "redux";
+
+
+//const initialState = { tech: "React " };
+//const store = createStore(reducer, initialState);
+
+class App extends Component {
+  // the state object has been removed. 
+  //it's gonna be redux instead.
+  // state = {
+  //   tech: "Redux"
+  // };
+
+  
+  render() {
+    return [
+      <HelloWorld key={1} tech={store.getState().tech} />,
+      <ButtonGroup key={2} technologies={["React", "Elm", "React-redux"]} />
+    ];
+  }
+}
+
+export default App;
